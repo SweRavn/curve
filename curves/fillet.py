eecc31b7ba6e15b0eb01fe81b81902792f4aa36b
Robert Rehammar <robert.rehammar@rar.nu>
2020-12-15 21:58:23 +0100

    Fixes ordering of fillet sweep

--- curves/fillet.py
+++ curves/fillet.py
@@ -11,7 +11,7 @@ from .curves.ellipse import Ellipse
 def fillet(p, p1, p2, r):
     """
     Create a fillet at point p defined by points p1 and p2 and r.
-    @return tripplet (e, a1, a2) where e is of the ellipse defining the fillet arc, ai is the touching point towards pi.
+    @return tripplet (e, a1, a2) where e is the arc of the ellipse defining the fillet arc, ai is the touching point towards pi.
     """
     p1 = p1 - p
     p2 = p2 - p
@@ -28,11 +28,10 @@ def fillet(p, p1, p2, r):
     a1 = a*t1 # Fillet touching point towards p1
     a2 = a*t2 # Fillet touching point towards p2
     
-#    r1 = a1 - c
-#    r2 = a2 - c
-    
-    s = (-c).arg + 2*pi # add 2 pi to be certain not to wrap over negative angles
+    s = (-c).arg# + 2*pi # add 2 pi to be certain not to wrap over negative angles
     s1 = s - alpha
     s2 = s + alpha
     
-    return (Ellipse(r=r, s1=min(s1,s2), s2=max(s1,s2), t=c+p), a1+p, a2+p)
+    d1, d2 = t1.arg, t2.arg # Used to sort the arguments
+    
+    return (Ellipse(r=r, s1=s2*(d1<=d2)+s1*(d2<d1), s2=s1*(d1<=d2)+s2*(d2<d1), t=c+p), a1+p, a2+p)
